a97a09342f9aba5aaf794e06e58f95dec08820b3
Sergey Seregov <seregov.sd@yandex.by>
2025-08-03 16:55:39 +0300

    refactor: use explicit jakarta.persistence imports

--- src/main/java/by/task/models/User.java
+++ src/main/java/by/task/models/User.java
@@ -1,6 +1,12 @@
 package by.task.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 
